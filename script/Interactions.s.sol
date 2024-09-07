f5839e4574683184055cf93b8c67cc085b31bff0
Mutualideas <jason.lu.nz@hotmail.com>
2024-09-07 22:56:02 +1000

    Restructure Test Directory & Makefile

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-// Fund
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
-import {Script} from "forge-std/Script.sol";
+contract FundFundMe is Script {
+    uint256 SEND_VALUE = 0.1 ether;
 
-contract FundFundMe is Script {}
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
 
-contract WithdrawFundMe is Script {}
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
